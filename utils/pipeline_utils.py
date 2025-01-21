396d043fe2176a092495606f776a1c5042765132
Squire-A <squire.anthony@gmail.com>
2025-01-21 17:41:37 +0000

    FIX: Re-ordered table creations in create_tables function

--- utils/pipeline_utils.py
+++ utils/pipeline_utils.py
@@ -6,5 +6,6 @@ def create_tables(engine):
     create_posts_table_query = get_sql_query('create_posts_table.sql')
     create_comments_table_query = get_sql_query('create_comments_table.sql')
     # Execute the queries to create the tables
-    execute_sql_transaction(create_comments_table_query, engine)
     execute_sql_transaction(create_posts_table_query, engine)
+    execute_sql_transaction(create_comments_table_query, engine)
+    
